5ea657cbf724fe4a764e565f0cf492f96b934821
Alicianonkonyana <133217174+Alicianonkonyana@users.noreply.github.com>
2024-06-18 22:21:29 +0200

    added basic layout of the page

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,4 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 export default function Todolist() {
-  return <div className="Todolist">Hello world</div>;
+  const [tasks, setTasks] = useState([]);
+  const [newTasks, setNewTasks] = useState("");
+  function handleInput(event) {
+    setTasks(event.target.value);
+  }
+
+  return (
+    <div className="Todolist">
+      <h1>To-Do-List</h1>
+      <input
+        type="text"
+        placeholder="write a task..."
+        className="task-input"
+        onChange={handleInput}
+      />
+      <button type="submit" className="add-button">
+        Add
+      </button>
+    </div>
+  );
 }
